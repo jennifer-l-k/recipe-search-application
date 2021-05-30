7cef3a4cd68452cf798713505090cfd28bf2371a
Jennifer Krüger <jennifer.krueger0400@gmail.com>
2021-05-30 20:28:58 +0200

    Expansion method read_json

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import pyjson5 as json # JSON5 Format für die Möglichkeit der Formatierungsänderung (Kommentare, Absätze, etc.)
+# JSON5 Format für die Möglichkeit der Formatierungsänderung (Kommentare, Absätze, etc.)
+import pyjson5 as json
 from dataclasses import dataclass
 import dataclasses
 
-with open('./recipes.json5',encoding='utf-8') as file:
+with open('./recipes.json5', encoding='utf-8') as file:
     data = json.load(file)
 
-@dataclass # Methode __init__() zur Erzeugung von Objekten wird eingespart
-class Ingredient: # Aufteilung der Zutaten
+
+@dataclass  # Methode __init__() zur Erzeugung von Objekten wird eingespart
+class Ingredient:  # Aufteilung der Zutaten
     """ ... """
     name: str
     specification: str
     amount: str
 
+
 @dataclass
-class Recipe: # Wiedergabe des Rezepts
+class Recipe:  # Wiedergabe des Rezepts
     """" ... """
     id: int
     name: str
@@ -21,44 +24,53 @@ class Recipe: # Wiedergabe des Rezepts
     difficulty: str
     preparation: str
     tip: str
-    ingredient: list[Ingredient] = dataclasses.field(default_factory=list) # Ausgabe der Zutaten in einer Liste
+    ingredients: list[Ingredient] = dataclasses.field(
+        default_factory=list)  # Ausgabe der Zutaten in einer Liste
 
-def readJSON():
-    for recipe in data["recipes"]:
-        #print("id: " + recipe["id"])
-        print("name: " + recipe["name"])
-        print("portion for: " + recipe["portion"])
-        print("difficulty: " + recipe["difficulty"])
-        print("preparation: " + recipe["preparation"])
-        print("tip: " + recipe["tip"])
-        #print("ingredients: " + recipe["ingredient"])
-        print("")
-print(readJSON())
-
-#def main(): # Testfunktion mit (von Hand) angelegtem Rezept, bevor JSON Daten eingebunden werden
-    #new_recipe = Recipe(
-        #id = 1,
-        #name = "Tomatensuppe",
-        #portion = "4 Personen",
-        #difficulty = "einfach",
-        #preparation = "...",
-        #tip = " ",
-    #)
-    #new_ingredient = Ingredient(
-        #name = "Tomaten",
-        #specification = " ",
-        #amount = "500 gr"
-    #)
-    #new_recipe.ingredient.append(new_ingredient) # Hinzufügen der Zutat 1
-    #new_ingredient = Ingredient(
-        #name = "Sahne",
-        #specification = " ",
-        #amount = "200 gr"
-    #)
-    #new_recipe.ingredient.append(new_ingredient) # Hinzufügen der Zutat 2
-    #new_recipe.ingredient.append(new_ingredient) # Hinzufügen der Zutat n
-    #print(new_recipe)
-#main()
+@dataclass
+class RecipesDatabase:
+    """ ... """
+    all_recipes_dict: dict = dataclasses.field(
+        default_factory = dict) 
+    ingredient_dict: dict = dataclasses.field(
+        default_factory = dict) 
 
 
-#food_dict = dict()
+    def read_json(self):
+        """ ... """
+        for recipe in data["recipes"]:
+            new_recipe = Recipe(id=recipe["id"],
+                                name=recipe["name"],
+                                portion=recipe["portion"],
+                                difficulty=recipe["difficulty"],
+                                preparation=recipe["preparation"],
+                                tip=recipe["tip"])
+
+            for ingredient in recipe["ingredient"]:
+                new_ingredient = Ingredient(ingredient["name"], ingredient.get(
+                    "specification", ""), ingredient["amount"])
+                new_recipe.ingredients.append(new_ingredient)
+
+                current_recipes = self.ingredient_dict.get(
+                    new_ingredient.name.casefold(), None)
+                if current_recipes is None:
+                    current_recipes = []
+                current_recipes.append(new_recipe.id)
+                self.ingredient_dict[new_ingredient.name.casefold()] = current_recipes
+            self.all_recipes_dict[new_recipe.id] = new_recipe  # Dictonary nach ID
+
+    
+    def search_recipes_by_ingredients(self,ingredient):
+        """ ... """
+        recipes = []
+        tmp = self.ingredient_dict[ingredient.casefold()]
+        for recipe_id in tmp:
+            recipes.append(self.all_recipes_dict[recipe_id])
+        return recipes
+
+
+db = RecipesDatabase()
+db.read_json()
+tmp = db.search_recipes_by_ingredients("nudeln")
+for recipe in tmp:
+    print(f"Id {recipe.id}: {recipe.name}")
