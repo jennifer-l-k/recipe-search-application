7e6d92108ab271e5b56aeda2279d864d6bdebb3b
Jennifer Krüger <jennifer.krueger0400@gmail.com>
2021-06-01 00:32:30 +0200

    Add exit button

--- gui.py
+++ gui.py
@@ -2,104 +2,150 @@ import tkinter as tk
 import tkinter.font as tkFont
 
 class App:
+    root: tk.Tk
+
     def __init__(self, root):
+        self.root = root
         #setting title
-        root.title("undefined")
+        self.root.title("Python Laborprojekt | Rezept Auswahl")
         #setting window size
         width=600
         height=500
-        screenwidth = root.winfo_screenwidth()
-        screenheight = root.winfo_screenheight()
+        screenwidth = self.root.winfo_screenwidth()
+        screenheight = self.root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        root.geometry(alignstr)
-        root.resizable(width=False, height=False)
+        self.root.geometry(alignstr)
+        self.root.resizable(width=False, height=False)
 
-        GLineEdit_194=tk.Entry(root)
-        GLineEdit_194["bg"] = "#a4a5a6"
-        GLineEdit_194["borderwidth"] = "1px"
-        GLineEdit_194["cursor"] = "arrow"
-        ft = tkFont.Font(family='Times',size=8)
-        GLineEdit_194["font"] = ft
-        GLineEdit_194["fg"] = "#ffffff"
-        GLineEdit_194["justify"] = "center"
-        GLineEdit_194["text"] = "Eingabe von Lebensmittel"
-        GLineEdit_194.place(x=410,y=110,width=169,height=30)
 
-        GButton_365=tk.Button(root)
-        GButton_365["bg"] = "#a4a5a6"
-        GButton_365["cursor"] = "mouse"
-        ft = tkFont.Font(family='Times',size=12)
-        GButton_365["font"] = ft
-        GButton_365["fg"] = "#ffffff"
-        GButton_365["justify"] = "center"
-        GButton_365["text"] = "Alle Rezepte anzeigen"
-        GButton_365.place(x=420,y=450,width=157,height=30)
-        GButton_365["command"] = self.GButton_365_command
-
-        GLabel_585=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=22)
-        GLabel_585["font"] = ft
-        GLabel_585["fg"] = "#333333"
-        GLabel_585["justify"] = "center"
-        GLabel_585["text"] = "Python Laborprojekt"
-        GLabel_585.place(x=180,y=30,width=256,height=53)
-
-        GLabel_163=tk.Label(root)
-        GLabel_163["bg"] = "#e2001a"
+        #Label for the DHBW Logo
+        label_banner=tk.Label(self.root)
+        label_banner["bg"] = "#e2001a"
+        label_banner["disabledforeground"] = "#e2001a"
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_163["font"] = ft
-        GLabel_163["fg"] = "#333333"
-        GLabel_163["justify"] = "center"
-        GLabel_163["text"] = ""
-        GLabel_163.place(x=0,y=0,width=599,height=30)
+        label_banner["font"] = ft
+        label_banner["fg"] = "#333333"
+        label_banner["justify"] = "center"
+        label_banner["text"] = ""
+        label_banner.place(x=0,y=0,width=599,height=30)
 
-        GLabel_211=tk.Label(root)
+
+        #Label with the Author within the Label for the DHBW Logo
+        label_author=tk.Label(self.root)
+        ft = tkFont.Font(family='Times',size=9)
+        label_author["bg"] = "#e2001a"
+        label_author["font"] = ft
+        label_author["fg"] = "#ffffff"
+        label_author["justify"] = "center"
+        label_author["text"] = "Erstellt von: Jennifer L. Krüger"
+        label_author.place(x=450,y=0,width=147,height=30)
+
+
+        #Label for the main header
+        label_mainheader=tk.Label(self.root)
+        ft = tkFont.Font(family='Times',size=22)
+        label_mainheader["font"] = ft
+        label_mainheader["fg"] = "#333333"
+        label_mainheader["justify"] = "center"
+        label_mainheader["text"] = "Python Laborprojekt"
+        label_mainheader.place(x=180,y=30,width=256,height=53)
+
+
+        #Label for the second header
+        label_subheader=tk.Label(self.root)
         ft = tkFont.Font(family='Times',size=16)
-        GLabel_211["font"] = ft
-        GLabel_211["fg"] = "#333333"
-        GLabel_211["justify"] = "center"
-        GLabel_211["text"] = "Rezept Auswahl"
-        GLabel_211.place(x=240,y=70,width=142,height=30)
+        label_subheader["font"] = ft
+        label_subheader["fg"] = "#333333"
+        label_subheader["justify"] = "center"
+        label_subheader["text"] = "Rezept Auswahl"
+        label_subheader.place(x=240,y=70,width=142,height=30)
 
-        GButton_221=tk.Button(root)
-        GButton_221["bg"] = "#a4a5a6"
-        GButton_221["cursor"] = "mouse"
+
+        #Entry-Widget for the Foodkeys
+        entry_enterfoodkey=tk.Entry(self.root)
+        entry_enterfoodkey["bg"] = "#cfcccc"
+        entry_enterfoodkey["borderwidth"] = "1px"
+        entry_enterfoodkey["cursor"] = "arrow"
         ft = tkFont.Font(family='Times',size=12)
-        GButton_221["font"] = ft
-        GButton_221["fg"] = "#ffffff"
-        GButton_221["justify"] = "center"
-        GButton_221["text"] = "Schließen"
-        GButton_221.place(x=20,y=450,width=157,height=30)
-        GButton_221["command"] = self.GButton_221_command
+        entry_enterfoodkey["font"] = ft
+        entry_enterfoodkey["fg"] = "#000000"
+        entry_enterfoodkey["justify"] = "center"
+        entry_enterfoodkey["text"] = "Eingabe "
+        entry_enterfoodkey.place(x=430,y=110,width=157,height=30)
 
-        GLabel_562=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=8)
-        GLabel_562["font"] = ft
-        GLabel_562["fg"] = "#ffffff"
-        GLabel_562["justify"] = "center"
-        GLabel_562["text"] = "Erstellt von: Jennifer L. Krüger"
-        GLabel_562.place(x=450,y=0,width=147,height=30)
 
-        GButton_418=tk.Button(root)
-        GButton_418["bg"] = "#a4a5a6"
+        #Button to reset all the Foodkey within the Entry-Widget
+        button_reset=tk.Button(self.root)
+        button_reset["bg"] = "#cfcccc"
         ft = tkFont.Font(family='Times',size=12)
-        GButton_418["font"] = ft
-        GButton_418["fg"] = "#ffffff"
-        GButton_418["justify"] = "center"
-        GButton_418["text"] = "Hilfe"
-        GButton_418.place(x=220,y=450,width=157,height=30)
-        GButton_418["command"] = self.GButton_418_command
-
-    def GButton_365_command(self):
-        print("command")
+        button_reset["font"] = ft
+        button_reset["fg"] = "#000000"
+        button_reset["justify"] = "center"
+        button_reset["text"] = "Eingabe Rücksetzen"
+        button_reset.place(x=430,y=150,width=157,height=30)
+        button_reset["command"] = self.button_reset_command
 
 
-    def GButton_221_command(self):
-        print("command")
+        #Button to close the Application
+        button_close=tk.Button(self.root)
+        button_close["bg"] = "#cfcccc"
+        ft = tkFont.Font(family='Times',size=12)
+        button_close["font"] = ft
+        button_close["fg"] = "#000000"
+        button_close["justify"] = "center"
+        button_close["text"] = "Schließen"
+        button_close.place(x=20,y=450,width=157,height=30)
+        print(self.button_close_command)
+        button_close["command"] = self.button_close_command
 
 
-    def GButton_418_command(self):
-        print("command")
+        #Button to show a Message for Using-Informations
+        button_help=tk.Button(self.root)
+        button_help["bg"] = "#cfcccc"
+        ft = tkFont.Font(family='Times',size=12)
+        button_help["font"] = ft
+        button_help["fg"] = "#000000"
+        button_help["justify"] = "center"
+        button_help["text"] = "Hilfe"
+        button_help.place(x=220,y=450,width=157,height=30)
+        button_help["command"] = self.button_help_command
+
+
+        #Button to show all the Recipes within the database
+        button_allrecipes=tk.Button(self.root)
+        button_allrecipes["bg"] = "#cfcccc"
+        ft = tkFont.Font(family='Times',size=12)
+        button_allrecipes["font"] = ft
+        button_allrecipes["fg"] = "#000000"
+        button_allrecipes["justify"] = "center"
+        button_allrecipes["text"] = "Alle Rezepte anzeigen"
+        button_allrecipes.place(x=420,y=450,width=157,height=30)
+        button_allrecipes["command"] = self.button_allrecipes_command
+
+
+    def button_allrecipes_command(self):
+        """ The method outputs a message within the command line 
+            indicating that it was executed successfully"""
+        print("Es wurden alle Rezepte erfolgreich angezeigt.")
+
+
+    def button_close_command(self):
+        """ The method outputs a message within the command line 
+            indicating that it was executed successfully"""
+        self.root.destroy()
+        print("Die Anwendung wurde geschlossen.")
+        
+
+    def button_help_command(self):
+        """ The method outputs a message within the command line 
+            indicating that it was executed successfully"""
+        print("Die Hilfeseite wurde aufgerufen.")
+
+
+    def button_reset_command(self):
+        """ The method outputs a message within the command line 
+            indicating that it was executed successfully"""
+        print("Die Lebensmitteleingabe wurde erfolgreich zurückgesetzt.")
 
 if __name__ == "__main__":
     root = tk.Tk()
